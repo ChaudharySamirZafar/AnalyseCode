e1ba21f80ff2eb85352612d60eb2776c13844efb
ChaudharySamirZafar <88251610+ChaudharySamirZafar@users.noreply.github.com>
2022-12-18 23:57:54 +0000

    Footer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Response from "./Pages/Code Analysis/Response/Response";
 import { CodeExplanationContextProvider } from "./Store/CodeExplanationContext";
 import SavedResponses from "./Pages/Saved Responses/SavedResponses";
 import EditorSelection from "./Pages/Code Analysis/EditorSelection/EditorSelection";
-import { useState } from "react";
+import { RiGithubFill, RiLinkedinBoxFill, RiDiscordFill, RiInstagramLine } from "react-icons/ri";
 
 function App() {
   return (
@@ -12,16 +12,31 @@ function App() {
       <CodeExplanationContextProvider>
         <div className="container">
           <div>
-            <EditorSelection/>
+            <EditorSelection />
           </div>
           <div className="editorColumn">
-            <Editor/>
+            <Editor />
           </div>
           <div className="response">
             <Response />
             <SavedResponses />
           </div>
         </div>
+        <footer className="footer">
+          <h3 className="contactMeSec">Contact Us</h3>
+          <h3 className="email">chaudharysamirzafar@gmail.com</h3>
+          <h3 className="socials">
+            <a href="https://www.linkedin.com/in/samir-zafar-6b59111bb/">
+              <RiLinkedinBoxFill />
+            </a>
+            <a href="https://github.com/ChaudharySamirZafar?tab=repositories">
+              <RiGithubFill />
+            </a>
+            <a href="https://www.instagram.com/samirr.zafarr/">
+              <RiInstagramLine />
+            </a>
+          </h3>
+        </footer>
       </CodeExplanationContextProvider>
     </div>
   );
