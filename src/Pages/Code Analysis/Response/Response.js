6f51553cf23f3c844c47574f5ada43288faca92c
ChaudharySamirZafar <88251610+ChaudharySamirZafar@users.noreply.github.com>
2022-12-19 15:43:49 +0000

    'Changed to POST method'

--- src/Pages/Code Analysis/Response/Response.js
+++ src/Pages/Code Analysis/Response/Response.js
@@ -29,7 +29,7 @@ const Response = () => {
     };
 
     fetch(EndPoints.Completions, {
-      method: "GET",
+      method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
